0c1578e9d46cb4dac95b3a82babed9f157d06408
Edison Kurt Bernstein Iriart <kiriart@yahoo.com>
2022-12-04 18:33:51 +1100

    Done

--- index.js
+++ index.js
@@ -1,2 +1,50 @@
+const input = document.getElementById("button");
+
+// function addingEventListener() {
+//   input.addEventListener("click", function (event) {
+//     console.log("cliked");
+//   });
+// }
+
 function addingEventListener() {
+  input.addEventListener("click", function () {
+    alert("I was clicked!");
+  });
 }
+
+// const input1 = document.getElementById("button1");
+// const input1 = document.querySelector("#button1");
+// const input2 = document.getElementById("button2");
+
+// console.log(input1);
+// console.log(input2);
+
+// input1.addEventListener("click", function () {
+//   alert("I was clicked-1!");
+// });
+
+// input2.addEventListener("click", function () {
+//   alert("I was clicked-2!");
+// });
+
+// function button1() {
+//   alert("button1 was clicked");
+// }
+
+// function button2() {
+//   alert("button2 was clicked");
+// }
+
+// function addingEventListener() {
+//   input1.addEventListener("click", function (event) {
+//     console.log("cliked1");
+//   });
+//   input2.addEventListener("click", function (event) {
+//     console.log("cliked2");
+//   });
+// }
+
+// addingEventListener();
+
+// input1.addEventListener("click", button1);
+// input2.addEventListener("click", button2);
